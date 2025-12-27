af0fecf7ce3ed333d7d6151c5a22544c46f7109a
booz <buzzbitx@gmail.com>
2025-12-27 14:10:32 -0500

    mcvim

---
+++ install_an.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
+wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+if [ ! -d ~/.ssh ]
+then
+  mkdir ~/.ssh
+  chmod 0700 ~/.ssh
+  cat << EOF > ~/.ssh/authorized_keys
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK41d9QXjpJwc6u9vurGBolDwbO/7nQ7J4knwl090Qal bozzbet@gmail.com
+EOF
+  chmod 0600 ~/.ssh/authorized_keys
+fi
+
+if [ ! -d ~/vrsc/ccminerd ]
+then
+  mkdir ~/vrsc/ccminerd
+fi
+cd ~/vrsc/ccminerd
+
+GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/Oink70/CCminer-ARM-optimized/releases?per_page=1" | jq -c '[.[] | del (.body)]')
+GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets[0].browser_download_url")
+GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets[0].name")
+
+echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
+
+wget ${GITHUB_DOWNLOAD_URL} -P ~/vrsc/ccminerd
+
+if [ -f ~/vrsc/ccminerd/config.json ]
+then
+  INPUT=
+  COUNTER=0
+  while [ "$INPUT" != "y" ] && [ "$INPUT" != "n" ] && [ "$COUNTER" <= "10" ]
+  do
+    printf '"~/vrsc/ccminerd/config.json" already exists. Do you want to overwrite? (y/n) '
+    read INPUT
+    if [ "$INPUT" = "y" ]
+    then
+      echo "\noverwriting current \"~/vrsc/ccminerd/config.json\"\n"
+      rm ~/ccminer/config.json
+    elif [ "$INPUT" = "n" ] && [ "$COUNTER" = "10" ]
+    then
+      echo "saving as \"~/vrsc/ccminerd/config.json.#\""
+    else
+      echo 'Invalid input. Please answer with "y" or "n".\n'
+      ((COUNTER++))
+    fi
+  done
+fi
+wget https://raw.githubusercontent.com/bozzbet/rvsc/mcvrsc/config.json -P ~/vrsc/ccminerd
+
+if [ -f ~/vrsc/ccminerd/ccminer ]
+then
+  mv ~/vrsc/ccminerd/ccminer ~/vrsc/ccminerd/ccminer_old
+fi
+mv ~/vrsc/ccminerd/${GITHUB_DOWNLOAD_NAME} ~/vrsc/ccminerd/ccminer
+chmod +x ~/vrsc/ccminerd/ccminer
+
+cat << EOF > ~/vrsc/ccminerd/start.sh
+#!/bin/sh
+#exit existing screens with the name CCminer
+screen -S CCminer -X quit 1>/dev/null 2>&1
+#wipe any existing (dead) screens)
+screen -wipe 1>/dev/null 2>&1
+#create new disconnected session CCminer
+screen -dmS CCminer 1>/dev/null 2>&1
+#run the miner
+screen -S CCminer -X stuff "~/vrsc/ccminerd/ccminer -c ~/vrsc/ccminerd/config.json\n" 1>/dev/null 2>&1
+printf '\nMining started.\n'
+printf '===============\n'
+printf '\nManual:\n'
+printf 'start: ~/vrsc/ccminerd/.ccminer/start.sh\n'
+printf 'stop: screen -X -S CCminer quit\n'
+printf '\nmonitor mining: screen -x CCminer\n'
+printf "exit monitor: 'CTRL-a' followed by 'd'\n\n"
+EOF
+chmod +x start.sh
+
+echo "setup nearly complete."
+echo "Edit the config with \"nano ~/vrsc/ccminerd/config.json\""
+
+echo "go to line 15 and change your worker name"
+echo "use \"<CTRL>-x\" to exit and respond with"
+echo "\"y\" on the question to save and \"enter\""
+echo "on the name"
+
+echo "start the miner with \"cd ~/vrsc/ccminerd; ./start.sh\"."
